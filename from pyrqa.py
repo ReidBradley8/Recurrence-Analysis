f1ccc2efbf0f335219627810f1d60704979df82c
ReidBradley8 <bradler4@mail.uc.edu>
2025-01-25 12:54:26 -0500

    Trying my best. I got some results but not sure how to interpret. The data was also changed along the way to be numeric but not sure how

--- from pyrqa.py
+++ from pyrqa.py
@@ -4,14 +4,28 @@ from pyrqa.neighbourhood import FixedRadius
 from pyrqa.metric import EuclideanMetric
 from pyrqa.computation import RQAComputation
 from pyrqa.analysis_type import Classic
+from pyrqa.recurrence_plot import RecurrencePlot
 import json
+import numpy as np
+from gensim.models import Word2Vec
+import matplotlib.pyplot as plt
 
 # Load data from JSON file
-with open('path/to/output.json', 'r') as file:
+with open(r"C:\Users\reidb\Documents\GitHub\Recurrence-Analysis\output.json", 'r') as file:
     data = json.load(file)
 
+# Assuming data is a list of words
+# Example: data = ["word1", "word2", "word3", ...]
+
+# Convert words to numerical vectors using Word2Vec
+model = Word2Vec([data], vector_size=100, window=5, min_count=1, workers=4)
+word_vectors = [model.wv[word] for word in data]
+
+# Flatten the list of vectors to create a single time series
+flattened_vectors = np.array(word_vectors).flatten()
+
 # Create TimeSeries object
-time_series = TimeSeries(data, embedding_dimension=2, time_delay=1)
+time_series = TimeSeries(flattened_vectors, embedding_dimension=2, time_delay=1)
 
 # Define settings
 settings = Settings(
@@ -28,3 +42,13 @@ result = computation.run()
 
 # Print results
 print(result)
+
+# Generate Recurrence Plot
+recurrence_plot = RecurrencePlot(settings)
+recurrence_matrix = recurrence_plot.recurrence_matrix()
+
+plt.imshow(recurrence_matrix, cmap='binary', origin='lower')
+plt.title('Recurrence Plot')
+plt.xlabel('Time')
+plt.ylabel('Time')
+plt.show()
